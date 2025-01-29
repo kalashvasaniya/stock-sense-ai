afe8e1970726e73a2fc79e6bfbbc52ad6b899eeb
kalash <88212870+kalashvasaniya@users.noreply.github.com>
2025-01-29 19:06:03 +0530

    Update page.js

--- app/Components/StockSummary/page.js
+++ app/Components/StockSummary/page.js
@@ -1,3 +1,4 @@
+"use client";
 import { useState } from 'react';
 import { Line, Bar } from 'recharts';
 import { AlertCircle, TrendingUp, TrendingDown, DollarSign, BarChart2, Activity } from 'lucide-react';
